4d12dcac1caf9c22b1a67e3671b72825b9c41f8b
Uday <iamudayhegde@gmail.com>
2025-03-02 22:06:42 +0530

    Modified Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,6 @@ public class Main {
     private static String resolverAddress = null;
 
     public static void main(String[] args) {
-        // Parse command-line arguments
         if (args.length > 0 && args[0].equals("--resolver")) {
             resolverAddress = args[1];
         }
