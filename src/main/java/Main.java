f511d169c4671bd4398ec4e2259c2c4c9190fc6c
Uday <iamudayhegde@gmail.com>
2025-03-02 22:20:02 +0530

    Modified Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,6 @@ import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -59,7 +58,7 @@ public class Main {
             DatagramPacket response = new DatagramPacket(responseBuffer, responseBuffer.length);
             socket.receive(response);
 
-            return Arrays.copyOfRange(response.getData(), 0, response.getLength());
+            return response.getData();
         } catch (IOException e) {
             System.err.println("Error forwarding DNS query: " + e.getMessage());
             return null;
@@ -68,7 +67,7 @@ public class Main {
 
     static byte[] handleDnsQuery(byte[] request) {
         DNSMessage dnsMessage = parseDnsQuery(request);
-        return buildDnsResponse(dnsMessage, request);
+        return buildDnsResponse(dnsMessage);
     }
 
     static DNSMessage parseDnsQuery(byte[] request) {
@@ -99,7 +98,7 @@ public class Main {
     private static String parseDomainName(ByteBuffer buffer, byte[] request) {
         StringBuilder domainName = new StringBuilder();
         int length;
-        int originalPos = buffer.position();
+        int initialPosition = buffer.position();
 
         while ((length = buffer.get() & 0xFF) != 0) {
             if ((length & 0xC0) == 0xC0) {
@@ -139,8 +138,9 @@ public class Main {
         return domainName.toString();
     }
 
-    static byte[] buildDnsResponse(DNSMessage dnsMessage, byte[] request) {
+    static byte[] buildDnsResponse(DNSMessage dnsMessage) {
         ByteBuffer buffer = ByteBuffer.allocate(512).order(ByteOrder.BIG_ENDIAN);
+
         buffer.putShort(dnsMessage.id);
         buffer.putShort((short) 0x8180);
         buffer.putShort((short) dnsMessage.questions.size());
@@ -160,10 +160,10 @@ public class Main {
             buffer.putShort((short) 1);
             buffer.putInt(3600);
             buffer.putShort((short) 4);
-            buffer.put((byte) 76);
-            buffer.put((byte) 76);
-            buffer.put((byte) 21);
-            buffer.put((byte) 21);
+            buffer.put((byte) 127);
+            buffer.put((byte) 0);
+            buffer.put((byte) 0);
+            buffer.put((byte) 2);
         }
 
         buffer.flip();
