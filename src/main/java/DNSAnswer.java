ef93fd4a55fe89fcd504998f7d6651752016b0fa
Uday <iamudayhegde@gmail.com>
2025-03-03 12:30:46 +0530

    Modified DNSAnswer

--- src/main/java/DNSAnswer.java
+++ src/main/java/DNSAnswer.java
@@ -3,43 +3,50 @@ import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 public class DNSAnswer {
-    private short QTYPE;
-    private short QCLASS;
-    private int TTL;
-    private short RDLENGTH;
+
+    // Fields representing the type, class, time-to-live, resource data length, question, answer, and length of the DNS answer
+    private short qType;
+    private short qClass;
+    private int ttl;
+    private short rdLength;
     private String question;
     private String answer;
     private int length;
 
-    public DNSAnswer(String question, short QTYPE, short QCLASS, short RDLENGTH, String answer) {
+    // Constructor to initialize a DNSAnswer object with question, qType, qClass, rdLength, and answer
+    public DNSAnswer(String question, short qType, short qClass, short rdLength, String answer) {
         this.question = question;
-        this.QTYPE = QTYPE;
-        this.QCLASS = QCLASS;
-        this.TTL = 60;
-        this.RDLENGTH = RDLENGTH;
+        this.qType = qType;
+        this.qClass = qClass;
+        this.ttl = 60;
+        this.rdLength = rdLength;
         this.answer = answer;
     }
 
+    // Method to get the byte array representation of the DNS answer
     public byte[] getAnswer() {
         ByteBuffer buffer = ByteBuffer.allocate(512);
         buffer.put(encodeDomainName(question));
-        buffer.putShort(QTYPE);
-        buffer.putShort(QCLASS);
-        buffer.putInt(TTL);
-        buffer.putShort(RDLENGTH);
+        buffer.putShort(qType);
+        buffer.putShort(qClass);
+        buffer.putInt(ttl);
+        buffer.putShort(rdLength);
         buffer.put(encodeIpAddress(answer));
         this.length = buffer.position();
         return Arrays.copyOf(buffer.array(), buffer.position());
     }
 
+    // Method to get the length of the DNS answer
     public int getAnswerLength() {
         return length;
     }
 
+    // Method to return a string representation of the DNS answer
     public String toString() {
-        return "Answer: " + question + ", " + QTYPE + ", " + QCLASS + ", " + answer;
+        return "Answer: " + question + ", " + qType + ", " + qClass + ", " + answer;
     }
 
+    // Private method to encode an IP address into a byte array
     private byte[] encodeIpAddress(String s) {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         if (s.length() == 0) return out.toByteArray();
@@ -49,6 +56,7 @@ public class DNSAnswer {
         return out.toByteArray();
     }
 
+    // Private method to encode a domain name into a byte array
     private byte[] encodeDomainName(String s) {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         for (String label : s.split("\\.")) {
